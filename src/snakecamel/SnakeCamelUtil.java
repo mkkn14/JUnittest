0bf8a50fbd1fb65f07b16721de490ee424b50340
g16913km <g16913km@gm.tsuda.ac.jp>
2018-05-15 14:14:53 +0900

    テスト成功

--- src/snakecamel/SnakeCamelUtil.java
+++ src/snakecamel/SnakeCamelUtil.java
@@ -1,8 +1,14 @@
 package snakecamel;
 
+
+
 public class SnakeCamelUtil {
 
+	
 	public static String snakeToCamelcase(String snake_case) {
+		if (snake_case==" ") {
+            return snake_case;
+        }
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < words.length /*- 1*/; i++) {
@@ -12,6 +18,9 @@ public class SnakeCamelUtil {
 	}
 	 
 	public static String camelToSnakecase(String camelcase) {
+		if (camelcase==" ") {
+            return camelcase;
+        }
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
 		for (int i = 0; i < camelcase.length(); i++) {
@@ -21,22 +30,23 @@ public class SnakeCamelUtil {
 				if (sb.length() > 0) {
 					sb.append("_");
 				}
+			
 				sb.append(Character.toLowerCase(c));
-				j = i;
+				j = i+1;
 			}
 		}
 		sb.append(camelcase.substring(j));
 		return new String(sb);
 	}
 	
-	static String capitalize(String s) {
+	public static String capitalize(String s) {
 		char first = s.charAt(0);
 		char upperFirst = Character.toUpperCase(first);
 		String rest = s.substring(1);
 		return upperFirst + rest;
 	}
 
-	static String uncapitalize(String s) {
+	public static String uncapitalize(String s) {
 		char first = s.charAt(0);
 		char lowerFirst = Character.toLowerCase(first);
 		String rest = s.substring(1);
